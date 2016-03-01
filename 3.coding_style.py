7f1ae49a9be2ffa9b6f79822904c127156453180
Wenhui Gao <gaowenhui2012@gmail.com>
2016-03-01 16:45:52 +0800

    Python编程规范介绍

--- 3.coding_style.py
+++ 3.coding_style.py
@@ -9,3 +9,19 @@
 # ---------------------------
 
 __author__ = 'deangao'
+
+'''
+
+保持良好的统一的编程风格是十分重要的。
+Google上面有很多关于各种语言的编程规范指导，Python也有自己的一些编程规范，
+PyCharm也会按一定的规范（比如PEP8）来检查编辑器中的代码。
+这里的编程风格有代码编写格式层面的也有代码逻辑组织层面的。
+
+https://github.com/iwhgao/zh-google-styleguide/blob/master/google-python-styleguide/python_language_rules.rst
+https://github.com/iwhgao/zh-google-styleguide/blob/master/google-python-styleguide/python_style_rules.rst
+
+好了，不多讲了，认真阅读上面的文档吧。
+
+'''
+
+
