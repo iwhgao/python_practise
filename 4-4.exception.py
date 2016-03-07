8e8536bdfadeb884fdcb4e9288300c90cc26d0e8
Wenhui Gao <gaowenhui2012@gmail.com>
2016-03-07 10:51:09 +0800

    Python异常处理--函数

--- 4-4.exception.py
+++ 4-4.exception.py
@@ -11,3 +11,88 @@
 __author__ = 'deangao'
 
 # TODO: try except finaly
+
+'''
+在设计程序时要考虑到可能发生的异常，并进行捕获，
+正确的处理异常以避免应为异常导致程序的崩溃，提高程序的鲁棒性。
+python中使用的是try except语法。
+Python中定义了各种异常，比如常见的类型不匹配、键不存在等。
+'''
+
+'''
+exception语法
+try:
+	<some code>
+exception (<some exception type1>, <some exception type2>)
+	<deal with exception>
+else:
+	<some code>
+finally:
+	<some code>
+
+或者try下面带多个exception
+exception <type1>, e1
+这里的e为异常的实例，可以被异常处理代码使用
+
+try:
+	<some code>
+exception <type1> as e1:
+	<deal with type1>
+exception <type2> as e2:
+	<deal with type2>
+'''
+
+
+# ---example1---
+def my_abs(num):
+	try:
+		num = float(num)
+	except ValueError, e:
+		print 'Error is', e
+	return num
+
+my_abs('sdfd')
+# 此处如果没有异常处理则会直接报错退出， 代码不健壮。
+# Error is could not convert string to float: sdfd
+
+print type(my_abs(-12))
+print type(my_abs('-12'))
+
+
+# ---example2---
+# else 表示在列举的error之外的处理
+# finally 表示最终都一定会执行的代码
+try:
+	float('aaa')
+except ValueError as e:
+	print 'ValueError', e
+except TypeError as e:
+	print 'TypeError', e
+else:
+	print 'No Error detected'
+finally:
+	print 'Finally print'
+
+# ---输出---
+'''
+ValueError could not convert string to float: aaa
+Finally print
+'''
+
+# ---example3---
+try:
+	float('12.3')
+except ValueError as e:
+	print 'ValueError', e
+except TypeError as e:
+	print 'TypeError', e
+else:
+	print 'No Error detected'
+finally:
+	print 'Finally print'
+
+# ---输出---
+'''
+No Error detected
+Finally print
+'''
--- 5.functions.py
+++ 5.functions.py
@@ -10,5 +10,115 @@
 
 __author__ = 'deangao'
 
-# TODO: lambda
-# TODO: pass
+'''
+函数可以简化代码，提高代码的复用率；
+先定义后调用
+'''
+
+'''
+Python中函数的基本格式
+
+def func_name(parm1, parm2=value, *parm3, **parm4):
+	<some code>
+
+其中的parm为参数名，可以赋予缺省值，
+可以传递变长列表， 也可以传递变长字典。
+
+下面一一介绍
+'''
+
+# ---example1 空函数---
+def my_print1():
+	pass
+
+# 调用该函数
+my_print1()
+
+# ---example2 无参函数---
+def my_print2():
+	print 'This is an empty function without parm'
+
+# 调用该函数
+my_print2()
+
+
+# ---example3 有参无缺省函数---
+def my_print3(toprint):
+	# 此处不限制参数的数据类型
+	print 'TO print:', toprint
+
+my_print3('Hello world')
+my_print3(2016)
+my_print3([1, 2, 3, 4])
+
+# ---example4 有参有缺省值函数---
+def my_print4(toprint='Hello'):
+	print 'TO print:', toprint
+
+#不传参时打印缺省值
+my_print4()
+my_print4('Hello world')
+
+# ---example5 变长参数函数 元组形式---
+# 此处用*来标注这个形式
+def my_print5(*topprint):
+	for element in topprint:
+		print 'To print', element
+
+my_print5(1)
+# 虽然定义时只定义了一个，但是在调用时传参的数可以是不定的
+my_print5(1, 2, 3, 4, 'hello')
+
+# ---example6 变长的字典参数---
+def my_print6(**mydict):
+	for key in mydict:
+		print key, mydict[key]
+
+my_print6(name='deangao', sez='m')
+
+# --example7 综合使用---
+def my_print7(one, *two, **three):
+	print 'One', one
+
+	for tmp in two:
+		print 'Two', tmp
+
+	for tmp in three:
+		print 'Three', tmp
+
+one = 'hello'
+two = (1, 3, 4, 5)
+three = {'name': 'deangao', 'sex': 'm'}
+
+# 也可以种方式来调用，省去了逐个传参
+my_print7(one, *two, **three)
+
+# 再讲讲函数的返回值  return
+def test1(name, sex):
+	return name, sex
+	# 也可以这样
+	return (name, sex)
+
+# 可以这样调用并返回结果
+name, sex = test1('deangao', 'm')
+print name, sex
+
+# 也可以这样
+(name, sex) = test1('deangao', 'm')
+print name, sex
+
+
+# =====================华丽的分割线====================
+'''
+函数式编程 lambda————相当于匿名函数
+只是lambda一般是返回值，即不能执行一些其它操作，比如print
+
+lambda语法格式
+lambda [parm1, parm2, ...]: expression
+
+返回的就是一个函数句柄
+这里简单介绍下
+'''
+
+a = lambda x: 'Element ' + x
+print a('hello world')
